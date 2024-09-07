d704db19f1ecce7ce1d4f79bfd84b0703ab714d2
Szilágyi Roland <szilagyiroland@Szilagyi-MacBook-Pro-2.local>
2024-09-07 22:00:41 +0200

    feat_btn-events-added

--- src/js/script.js
+++ src/js/script.js
@@ -9,8 +9,19 @@ const buttonStart = document.querySelector('.js-btn-start');
 
 // EVENTS
 buttonNext.addEventListener('click', runNext);
-
 function runNext() {
   pageHome.classList.add('hidden');
   pageOptions.classList.remove('hidden');
+  buttonNext.classList.add('hidden');
+  buttonHome.classList.remove('hidden');
+  buttonStart.classList.remove('hidden');
+}
+
+buttonHome.addEventListener('click', runHome);
+function runHome() {
+  pageHome.classList.remove('hidden');
+  pageOptions.classList.add('hidden');
+  buttonNext.classList.remove('hidden');
+  buttonHome.classList.add('hidden');
+  buttonStart.classList.add('hidden');
 }
