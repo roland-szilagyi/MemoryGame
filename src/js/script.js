d71f2e68bec804e7e0b2efd9cdc4027afcc56551
Szilágyi Roland <szilagyiroland@Szilagyi-MacBook-Pro-2.local>
2024-09-08 05:13:42 +0200

    feat_add:page-to-page_js-logic

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,35 @@
 // PAGES
-let pageOptions = document.querySelector('.js-options');
-let pageHome = document.querySelector('.js-home');
+const pageHome = document.querySelectorAll('.js-home');
+const pageOptions = document.querySelectorAll('.js-options');
+const pageGame = document.querySelectorAll('.js-game');
 
 // BUTTONS
 const buttonNext = document.querySelector('.js-btn-next');
 const buttonHome = document.querySelector('.js-btn-home');
 const buttonStart = document.querySelector('.js-btn-start');
+const buttonStop = document.querySelector('.js-btn-stop');
 
-// EVENTS
+// PAGE LOADERS
 buttonNext.addEventListener('click', runNext);
 function runNext() {
-  pageHome.classList.add('hidden');
-  pageOptions.classList.remove('hidden');
-  buttonNext.classList.add('hidden');
-  buttonHome.classList.remove('hidden');
-  buttonStart.classList.remove('hidden');
+  pageHome.forEach(element => element.classList.remove('active')); // elveszi az összes active-ot.
+  pageOptions.forEach(element => element.classList.add('active')); // hozzaadja az active-ot az összes megadott-hoz.
 }
 
 buttonHome.addEventListener('click', runHome);
 function runHome() {
-  pageHome.classList.remove('hidden');
-  pageOptions.classList.add('hidden');
-  buttonNext.classList.remove('hidden');
-  buttonHome.classList.add('hidden');
-  buttonStart.classList.add('hidden');
+  pageOptions.forEach(element => element.classList.remove('active')); // elveszi az összes active-ot.
+  pageHome.forEach(element => element.classList.add('active')); // hozzaadja az active-ot az összes megadott-hoz.
+}
+
+buttonStart.addEventListener('click', runStart);
+function runStart() {
+  pageOptions.forEach(element => element.classList.remove('active')); // elveszi az összes active-ot.
+  pageGame.forEach(element => element.classList.add('active')); // hozzaadja az active-ot az összes megadott-hoz.
+}
+
+buttonStop.addEventListener('click', runStop);
+function runStop() {
+  pageGame.forEach(element => element.classList.remove('active')); // elveszi az összes active-ot.
+  pageOptions.forEach(element => element.classList.add('active')); // hozzaadja az active-ot az összes megadott-hoz.
 }
