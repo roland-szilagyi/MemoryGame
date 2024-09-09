0e0612fd95b4c60e70e4466de8e30f0f369d8533
Szilágyi Roland <szilagyiroland@szilagyi-macbook-pro.home>
2024-09-09 22:14:28 +0200

    feat_function-generateCardNumbers-added

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+/* ---------- PAGES NAVIGATION ---------- */
 // PAGES
 const pageHome = document.querySelectorAll('.js-home');
 const pageOptions = document.querySelectorAll('.js-options');
@@ -33,3 +34,36 @@ function runStop() {
   pageGame.forEach(element => element.classList.remove('active')); // elveszi az összes active-ot.
   pageOptions.forEach(element => element.classList.add('active')); // hozzaadja az active-ot az összes megadott-hoz.
 }
+
+/* ---------- GAME PLAY ---------- */
+
+/**
+ * legenerálja a kártyaértékeket 1-töl 13-ig.
+ * @param Array of '1' ... '13'
+ */
+function generateCardNumbers() {
+  let cardNumbers = [];
+  for ( let i = 1; i <= 13; i++ ) {
+    cardNumbers.push(i);
+    cardNumbers.push(i);
+  };
+  return cardNumbers;
+};
+
+function cardShuffler() {
+
+}
+
+function cardRender() {
+
+}
+
+console.log( generateCards() );
+
+/* 
+  1. KÁRTYÁK AZ ASZTALRA - ALFELADATOK:
+  - generateCards: legenerál 3 kártyaértéket
+  - cardDoubler: megduplázza a kártyaértékeket
+  - cardShuffler: megkeveri a kártyaértékeket
+  - cardRender: megjeleníti a kártyákat
+*/
